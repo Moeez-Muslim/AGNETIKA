b6d595185fef75726d90f73f736cf16b807e0109
Moeez-Muslim <mail2moeezmuslim@gmail.com>
2024-11-24 03:03:14 +0500

    Added example.secrets.ts

---
+++ example.secrets.ts
@@ -0,0 +1,19 @@
+// Create a secrets.ts file with the follwing format
+const TRELLO_API_KEY = ""; 
+const TRELLO_API_SECRET = "";
+const TRELLO_API_TOKEN = "";
+const GOOGLE_CALENDAR_ID=""
+const GOOGLE_API_KEY=""
+const GOOGLE_SERVICE_ACCOUNT_EMAIL=""
+const GOOGLE_PRIVATE_KEY=""
+const secrets = {
+  TRELLO_API_KEY,
+  TRELLO_API_SECRET,
+  TRELLO_API_TOKEN,
+  GOOGLE_CALENDAR_ID,
+  GOOGLE_API_KEY,
+  GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  GOOGLE_PRIVATE_KEY
+};
+
+export default secrets;
